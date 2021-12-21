deb2672e089b649df8a9589b34ae411ade7eb41c
Sean McPherson <git@seanmcp.com>
2021-12-21 11:20:43 -0500

    backup

--- dispatch.js
+++ dispatch.js
@@ -2,18 +2,24 @@ export function Dispatch() {
   const initEventName = "dispatch-init";
   return {
     init() {
+      const innerHTML = HTMLElement.prototype.innerHTML;
+      HTMLElement.prototype.innerHTML = function () {
+        window.dispatchEvent(new CustomEvent(initEventName));
+        innerHTML.apply(this, arguments);
+      };
+
       window.addEventListener(initEventName, this.addListeners);
       window.dispatchEvent(new CustomEvent(initEventName));
       //   this.addListeners();
-      const observer = new MutationObserver((mutationsList) => {
-        for (const mutation of mutationsList) {
-          if (mutation.type === "childList" || mutation.type === "subtree") {
-            window.dispatchEvent(new CustomEvent(initEventName));
-            //   this.addListeners();
-          }
-        }
-      });
-      observer.observe(document.body, { childList: true, subtree: false });
+    //   const observer = new MutationObserver((mutationsList) => {
+    //     for (const mutation of mutationsList) {
+    //       if (mutation.type === "childList" || mutation.type === "subtree") {
+    //         window.dispatchEvent(new CustomEvent(initEventName));
+    //         //   this.addListeners();
+    //       }
+    //     }
+    //   });
+    //   observer.observe(document.body, { childList: true, subtree: false });
     },
     addListeners() {
       console.log("Setting up event listeners");
@@ -25,10 +31,10 @@ export function Dispatch() {
           detail = JSON.parse(node.dataset.dispatchDetail);
         }
         function listener() {
-            // TODO: Find a way to prevent default
-            node.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
+          // TODO: Find a way to prevent default
+          node.dispatchEvent(new CustomEvent(name, { bubbles: true, detail }));
         }
-        node.removeEventListener(type, listener)
+        node.removeEventListener(type, listener);
         node.addEventListener(type, listener);
         // node.removeAttribute("data-dispatch");
       });
